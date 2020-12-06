c50f8888c614e6e9465245f454ff885647b3f5d7
LeonDCd <17307130188@fudan.edu.cn>
2020-12-06 14:33:38 +0800

    updateState

---
+++ src/main/java/com/fduexchange/dao/OrderTableMapper.java
@@ -0,0 +1,20 @@
+package com.fduexchange.dao;
+
+import com.fduexchange.pojo.OrderTable;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+public interface OrderTableMapper {
+
+    int insert(OrderTable record);
+
+    @Select("select * from OrderTable where seller_id=#{seller_id}")
+    List<OrderTable> selectBySellerId(int seller_id);
+
+    @Select("select * from OrderTable where purchaser_id=#{purchaser_id}")
+    List<OrderTable> selectByPurchaserId(int purchaser_id);
+
+    int updateState(OrderTable record);
+}
